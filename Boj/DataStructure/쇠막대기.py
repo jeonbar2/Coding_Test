e14fea663ddfb1c37e0f92de7679ff52bfee0e4b
jeonho <jhkang1995@naver.com>
2022-08-09 15:31:28 +0900

    .

--- Boj/DataStructure/쇠막대기.py
+++ Boj/DataStructure/쇠막대기.py
@@ -1,1 +1,12 @@
 #백준 10799번
+# () -> 레이져
+# (...) ->쇠막대기
+
+arr=input()
+stack=[]
+for i in arr:
+    if i =="(":
+        stack.append(i)
+    elif i == ")":
+        stack.pop(-1)
+        print("괄호완성")
