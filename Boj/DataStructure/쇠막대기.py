51b6546379e5729bae8f57b87422e76a1bdaaa82
jeonho <jhkang1995@naver.com>
2022-08-11 16:06:38 +0900

    백준 10799번

--- Boj/DataStructure/쇠막대기.py
+++ Boj/DataStructure/쇠막대기.py
@@ -4,9 +4,25 @@
 
 arr=input()
 stack=[]
-for i in arr:
-    if i =="(":
-        stack.append(i)
-    elif i == ")":
-        stack.pop(-1)
-        print("괄호완성")
+answer=0
+index = (len(arr))
+# for i in arr:
+#     if i =="(":
+#         stack.append(i)
+#     elif i == ")":
+#         stack.pop(-1)
+#         print("괄호완성")
+#         print("남아있는 스택개수")
+#         answer+=(len(stack))
+for i in range(index):
+    if arr[i]== "(":
+        stack.append(arr[i])
+    elif arr[i]== ")":
+        if arr[i-1]=="(":
+            stack.pop(-1)
+            answer+=len(stack)
+        else:
+            stack.pop(-1)
+            answer+=1
+print(answer)
+
