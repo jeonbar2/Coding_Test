2c8ee27e87ab5e95fe34183c16769a7eac1d1f76
jeonho <jhkang1995@naver.com>
2022-08-18 17:14:43 +0900

    백준 2800번

---
+++ Boj/DataStructure/괄호제거.py
@@ -0,0 +1,31 @@
+# (0/(0))
+# 예제 출력 1
+# (0/0)
+# 0/(0)
+# 0/0
+from itertools import combinations
+ex=input()
+# ex = [ ( 0 / ( 0 ) ) ]
+stack=[]
+couple=[]
+answer=set()
+for idx,i in enumerate(ex):
+    if i=="(" :
+        stack.append(idx)
+    elif i==")":
+        start = stack.pop()
+        stop = idx
+        couple.append([start,stop]) # 알맞은 괄호 쌍을 찾아 index를 저장.
+
+for i in range(1,len(couple)+1):
+    combi = combinations(couple,i) #combination을 이용하여 뺴줄 조합의 개수를 구한다.
+    for j in combi:
+        tmp = list(ex)            #tmp에 원래 식을 저장해주고
+        for k in j:               #제거해줄 idx를 ''로 치환해준다.
+            start,end = k
+            tmp[start] = ''
+            tmp[end] = ''
+        answer.add(''.join(tmp))
+
+for i in sorted(list(answer)):
+    print(i)
