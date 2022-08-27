e6fa79d3a8b8aff3fa176b687fb1394f6e9d893b
jeonho <jhkang1995@naver.com>
2022-08-27 17:07:05 +0900

    백준 1620번

---
+++ Boj/DataStructure2/포켓몬마스터.py
@@ -0,0 +1,20 @@
+#백준 1620번
+import sys
+input = sys.stdin.readline
+n, m = map(int, input().split())
+
+dict = {}
+
+for i in range(1, n + 1):
+    a = input().rstrip()
+    dict[i] = a
+    dict[a] = i
+
+for i in range(m):
+    ans=input().rstrip()
+    if ans.isdigit():
+        print(dict[int(ans)])
+    else:
+        print(dict[ans])
+
+#dictionary를 사용용
