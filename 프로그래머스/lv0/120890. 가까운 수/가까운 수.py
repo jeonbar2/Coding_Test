3527869b8a0452ed762bd4ae4d50483ced1930e1
Jeonho Kang <jhkang1995@naver.com>
2022-11-24 02:19:10 +0900

    [level 0] Title: 가까운 수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120890. 가까운 수/가까운 수.py
@@ -0,0 +1,10 @@
+def solution(array, n):
+    answer = 0
+    array.sort()
+    answer = 0
+    com = n+100
+    for i in array:
+        if abs(i-n) < com:
+            com = abs(i-n)
+            answer = i
+    return answer
