ccd5c1ab793ca5008e1e8e7ac70fa84f3f1362e8
Jeonho Kang <jhkang1995@naver.com>
2023-02-21 15:45:25 +0900

    [Silver III] Title: 바이러스, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,41 +1,25 @@
-def dfs_recursive(node, visited,graph):
-    # 방문처리
-    visited.append(node)
+computers = int(input())
+nodes = {}
+for i in range(1, computers + 1):
+    nodes[i] = []
 
-    # 인접 노드 방문
-    for adj in graph[node]:
-        if adj not in visited:
-            dfs_recursive(adj, visited,a)
-
-    return len(visited)-1
-def dfs_stack(start,graph):
-    visited = []
-    # 방문할 순서를 담아두는 용도
-    stack = [start]
-
-    # 방문할 노드가 남아있는 한 아래 로직을 반복한다.
-    while stack:
-        # 제일 최근에 삽입된 노드를 꺼내고 방문처리한다.
-        top = stack.pop()
-
-        if top not in visited:
-            visited.append(top)
-        # 인접 노드를 방문한다.
-        for adj in graph[top]:
-            if adj not in visited:
-                stack.append(adj)
-
-    return len(visited)-1
-a={}
-
-n=int(input())
-for i in range(1,n+1):
-    a[i]=[]
-m=int(input())
+m = int(input())
 for i in range(m):
-    x,y=map(int,input().split())
-    a[x].append(y)
-    a[y].append(x)
+    x, y = map(int, input().split())
+    nodes[x].append(y)
+    nodes[y].append(x)
 
 
-print((dfs_recursive(1,[],a)))
+visited = []
+stack = [1]
+
+while stack:
+    pre = stack.pop()
+
+    if pre not in visited:
+        visited.append(pre)
+    for next in nodes[pre]:  # 다음 인접 노드 방문
+        if next not in visited:  # 방문 하지 않았으면
+            stack.append(next)  # 스택에 추가
+
+print(len(visited) - 1)
