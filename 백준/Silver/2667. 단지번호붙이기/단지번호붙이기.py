083f513ee298402d983c11be4b1f31198626155e
Jeonho Kang <jhkang1995@naver.com>
2023-02-28 10:39:08 +0900

    [Silver I] Title: 단지번호붙이기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,16 +1,16 @@
 n = int(input())
-grid = []
+apt = []
 for i in range(n):
-    grid.append(list(map(int, input())))
+    apt.append(list(map(int, input())))
 dx = [0, 0, 1, -1]
 dy = [1, -1, 0, 0]
-rows, cols = len(grid), len(grid[0])
+rows, cols = len(apt), len(apt[0])
 cnt = 0
 count = 0
-a = []
+ans = []
 for row in range(rows):
     for col in range(cols):
-        if grid[row][col] != 1:
+        if apt[row][col] != 1:
             continue
 
         cnt += 1
@@ -19,19 +19,20 @@ for row in range(rows):
         count = 0
         while stack:
             x, y = stack.pop()
-            if grid[x][y] != 0:
+            if apt[x][y] != 0:
                 count += 1
-            grid[x][y] = 0
+            apt[x][y] = 0
 
             for i in range(4):
                 nx = x + dx[i]
                 ny = y + dy[i]
-                if nx < 0 or nx >= rows or ny < 0 or ny >= cols or grid[nx][ny] != 1:
+                if nx < 0 or nx >= rows or ny < 0 or ny >= cols or apt[nx][ny] != 1:
                     continue
                 stack.append((nx, ny))
-        a.append(count)
+        ans.append(count)
+
 
 print(cnt)
-answer = sorted(a)
+answer = sorted(ans)
 for i in answer:
     print(i)
