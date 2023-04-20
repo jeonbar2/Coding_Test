50e9ba7581823a7d035b6f59f84b36444bb60eee
Jeonho Kang <jhkang1995@naver.com>
2023-04-20 16:22:39 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 300 ms, Memory: 32036 KB -BaekjoonHub

--- 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.py
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.py
@@ -1,19 +1,19 @@
 import sys
+
 input = sys.stdin.readline
 
 string = input().rstrip()
+
 word_stack = []
 tag = False
-res = ''
+res = ""
 
 for s in string:
-    # 공백이면 word_stack 비움
     if s == " ":
         while word_stack:
             res += word_stack.pop()
         res += s
 
-    # 태그 안 단어는 뒤집지 않음
     elif s == "<":
         while word_stack:
             res += word_stack.pop()
@@ -27,13 +27,9 @@ for s in string:
     elif tag:
         res += s
 
-    # 태그 밖 단어는 뒤집음
     else:
         word_stack.append(s)
-
-    # print(res, tag)
-
-# 출력
+        
 while word_stack:
     res += word_stack.pop()
 print(res)
